57c5fed7ee4142a7fc4ab350df53392f25e9f4cf
joekoul <joekanku@hotmail.com>
2018-03-26 12:42:04 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+//adding a strike onclick checkbox
+$(document).on('change', '.checkbox', function() 
+	{
+		if($(this).attr('checked')) 
+		{
+			$(this).removeAttr('checked');
+		} 
+		else 
+		{
+			$(this).attr('checked', 'checked');
+		}
+
+		$(this).parent().toggleClass('completed');
+		
+	});
+// remove task from list 
+	$(document).on('click', '.remove', function() 
+	{
+		$(this).parent().remove();
+		
+	});
+/*	// sortable list  drag and drop
+$( function() {
+    $( "#sortable" ).sortable();
+    $( "#sortable" ).disableSelection();
+  } );*?
+
