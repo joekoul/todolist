2abd3e56e16265c61715c4c3a35ee250ac00671d
joekoul <joekanku@hotmail.com>
2018-03-29 10:18:44 +0300

    Delete retrieve.php

--- retrieve.php
+++
@@ -1,9 +0,0 @@
-
-    
-        <?php 
-        // select all tasks if page is visited or refreshed
-        $tasks = mysqli_query($db, "SELECT * FROM tasks");
-//displaying task from database
-        $i = 1;
-         while ($row = mysqli_fetch_array($tasks)) { ?>
-        <?php $i++; } ?>
